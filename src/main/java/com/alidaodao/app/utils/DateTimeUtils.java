91d17b095b7fcaed45ed83b4b5bde53a902ee4cb
jack <ssliunian@yeah.net>
2022-08-02 20:38:41 +0800

    add date time utils

---
+++ src/main/java/com/alidaodao/app/utils/DateTimeUtils.java
@@ -0,0 +1,47 @@
+package com.alidaodao.app.utils;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * @author jack
+ * @since 2022-08-02
+ */
+public class DateTimeUtils {
+
+    /**
+     * 默认月份格式
+     */
+    public static final String MONTH_DEFAULT_FORMAT = "yyyy-MM";
+
+    /**
+     * 默认日期格式
+     */
+    public static final String DATE_DEFAULT_FORMAT = "yyyy-MM-dd";
+
+    /**
+     * 其他日期格式
+     */
+    public static final String DATE_BASE_FORMAT = "yyyy/MM/dd";
+
+    /**
+     * 默认日期时间格式
+     */
+    public static final String DATETIME_DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATETIME_MINUTE_FORMAT = "yyyy-MM-dd HH:mm";
+
+    /**
+     * 取默认的系统zone id
+     */
+    private static final ZoneId ZONE_ID = ZoneId.systemDefault();
+    /**
+     * 转换LocalDate为Date
+     * @param localDate 日期
+     * @return 响应结果
+     */
+    public static Date localDateToUtilDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+}
